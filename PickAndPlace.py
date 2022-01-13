2dcf62f7878f8e59ab923c26912a59711ea20e1d
Zane D. R <zane.reick@unknownts.tk>
2022-01-13 14:56:21 -0600

    Made some new changes

--- PickAndPlace.py
+++ PickAndPlace.py
@@ -5,18 +5,28 @@ bot = DBA(250, 0, 50)
 
 bot.dobotConnect()
 
+
 bot.moveHome()
 
 bot.pickToggle(-56)
 bot.toggleSuction()
 bot.pickToggle(-56)
 
-for x in range(4):
+for x in range(3):
+    newLoc = 25 * x
     bot.moveArmXY(125, -225)
+    bot.moveArmXY(newLoc, -225)
     bot.pickToggle(-56)
     bot.toggleSuction()
     bot.pickToggle(-56)
+    bot.moveArmXY(125, -225)
     bot.moveHome()
+    time.sleep(0.5)
+    bot.moveArmXY(125, -225)
+    bot.moveArmXY(newLoc, -225)
+    bot.pickToggle(-56)
+    bot.toggleSuction()
+    bot.pickToggle(-56)
 else:
     bot.pickToggle(-56)
     bot.toggleSuction()
